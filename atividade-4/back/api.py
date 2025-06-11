0ac124882fb61716a9b5d9c1aef32ebdfbd93109
gabitaquegami <g.taquegami@gmail.com>
2025-06-10 22:09:52 -0300

    minor changes

--- atividade-4/back/api.py
+++ atividade-4/back/api.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask import request, jsonify
-from ms.reserva import {
-    cancelar_reserva(),
-    interesses_promocoes
-}
+from ms.reserva import cancelar_reserva, interesses_promocoes
 
 app = Flask(__name__)
 
